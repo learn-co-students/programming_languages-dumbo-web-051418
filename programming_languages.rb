0147eb22c43bac9c78d76075112b214e3fa783bc
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-02 05:32:13 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,62 @@
+def languages
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+end
+
+def lang_list
+selected_langs = languages.collect do |type, lang_name|
+   lang_name.keys
+ end
+ selected_langs.flatten
+end
+
+def get_style(name)
+  type = []
+  type << :oo if languages[:oo].has_key?(name)
+  type << :functional if languages[:functional].has_key?(name)
+  type
+end
+
+def get_type(name)
+  languages[get_style(name)[0]][name][:type]
+end
+
 def reformat_languages(languages)
-  # your code here
+  formatted = {}
+  lang_list.collect do |lang|
+    formatted[lang] = {
+      type: get_type(lang),
+      style: get_style(lang)
+    }
+  end
+  formatted
 end
