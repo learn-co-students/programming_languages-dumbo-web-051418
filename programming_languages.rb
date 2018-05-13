472e85f8208daecc37821298c7b4038baf6345da
mrstevenaweiss <weiss.steven@gmail.com>
2018-05-13 15:18:19 -0400

    Done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+# 2-language => level 1
+# 3-:type => level 2
+# paradigm => values in array for new hash type in level 2
+# Desired Output: reformat_languages[:ruby] = { :type => "interpreted", :style => [:oo] }
+
+
 def reformat_languages(languages)
-  # your code here
+  reformat_languages = {}
+
+  languages.each do |paradigm, hash_as_value|
+    hash_as_value.each do |language, details|
+      reformat_languages[language] ||= details
+      reformat_languages[language][:style] ||= []
+      reformat_languages[language][:style] << paradigm
+    end
+  end
+
+  reformat_languages
 end
